e57e3e5a6a176b0249cd025eb14dda31b8839765
NikitaYakuntsev <nikitayakuntsev@yandex.ru>
2019-08-17 19:16:53 +0300

    Highlights improvements

--- src/main/java/org/nktyknstv/rfop/parser/service/DiffService.java
+++ src/main/java/org/nktyknstv/rfop/parser/service/DiffService.java
@@ -4,14 +4,19 @@ import com.github.difflib.algorithm.DiffException;
 import com.github.difflib.text.DiffRow;
 import com.github.difflib.text.DiffRowGenerator;
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @UtilityClass
 public class DiffService {
 
+    private Pattern WORDS_REGEX = Pattern.compile("([А-Яа-яЁё].*[А-Яа-яЁё])");
 
 
     public String calcDiff(String before, String after) {
@@ -23,7 +28,7 @@ public class DiffService {
                     .showInlineDiffs(true)
 //                    .mergeOriginalRevised(true)
                     .inlineDiffByWord(true)
-                    .oldTag(s -> s ? "<s>" : "</s>")
+                    .oldTag(s -> "")
                     .newTag(s -> s ? "<mark>" : "</mark>")
                     .build();
             List<DiffRow> diffRows = generator.generateDiffRows(
@@ -31,13 +36,31 @@ public class DiffService {
                     prepareInput(after)
             );
             return diffRows.stream()
-                    .map(DiffRow::getNewLine)
+                    .map(DiffService::getChangeRow)
+                    .filter(Objects::nonNull)
                     .collect(Collectors.joining("<br>"));
 //                    .replaceAll("&lt;", "<")
 //                    .replaceAll("&gt;", ">");
         } catch (DiffException e) {
             return "";
         }
+    }
+
+    private String getChangeRow(DiffRow diffRow) {
+        if (diffRow.getTag() == DiffRow.Tag.DELETE) {
+            return getDeletedLineFormatted(diffRow.getOldLine());
+        }
+        return diffRow.getNewLine();
+    }
+
+    private String getDeletedLineFormatted(String oldLine) {
+        Matcher oldMatcher = WORDS_REGEX.matcher(oldLine);
+        if (oldMatcher.find()) {
+            String russianTextOld = oldMatcher.group();
+            int len = Math.min(russianTextOld.length() / 3, 30);
+            return "<s>" + StringUtils.left(russianTextOld, len) + " ... " + StringUtils.right(russianTextOld, len) + "</s>";
+        }
+        return null;
     }
 
     public String prepareDiffsReport(List<DiffRow> diffRows) {
@@ -58,10 +81,12 @@ public class DiffService {
     }
 
     public List<String> prepareInput(String input) {
-        return Arrays.asList(input.replaceAll("<s>", "")
+        return Arrays.asList(input
+                .replaceAll("<s>", "")
 //                .replaceAll("\n", "<br>")
                 .replaceAll("</s>", "")
                 .replaceAll("<mark>", "")
-                .replaceAll("</mark>", ""));
+                .replaceAll("</mark>", "")
+        .split("\n"));
     }
 }
